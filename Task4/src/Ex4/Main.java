06928ad48ec3a40365d892bac8cbfa7c80fb11c9
VladimirZ00Z <94064126+VladimirZ00Z@users.noreply.github.com>
2022-04-12 19:50:56 +0300

    Add files via upload

---
+++ Task4/src/Ex4/Main.java
@@ -0,0 +1,19 @@
+package Ex4;
+public class Main {
+    public static void main(String[] args) {
+            System.out.println("\nПРАКТИКА 4, ЧАСТЬ 1");
+            Cycles.taskOne();
+            Cycles.taskTwo();
+            System.out.println(Cycles.taskThree());
+            System.out.println(Cycles.taskFor());
+            System.out.println(Cycles.taskFive());
+            Cycles.taskSix();
+
+            System.out.println("\nПРАКТИКА 4, ЧАСТЬ 2");
+            ArrayClass.task1();
+            ArrayClass.task2();
+            ArrayClass.task3();
+            ArrayClass.task4();
+            ArrayClass.task5();
+    }
+}
