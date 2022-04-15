4824eb90b9ea30b5d81be4d09a9463a895c86a29
VladimirZ00Z <94064126+VladimirZ00Z@users.noreply.github.com>
2022-04-15 18:41:22 +0300

    Update Main.java

--- Task6/src/Task3/Main.java
+++ Task6/src/Task3/Main.java
@@ -2,7 +2,7 @@ package Task3;
 
 public class Main {
     public static void main(String[] args) {
-        Truck truck = new Truck(7000, "Êàìàç", 'w', 90, 6,9000);
+        Truck truck = new Truck(7000, "Truck", 'w', 90, 6,9000);
         truck.outPut();
         truck.newWheels(8);
     }
